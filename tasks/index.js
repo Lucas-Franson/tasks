3cb3cd9257507289a0ae0a3608135554ac890346
Lucas Franson <34326338+Lucas-Franson@users.noreply.github.com>
2019-04-29 00:00:26 -0300

    Add files via upload

--- tasks/index.js
+++ tasks/index.js
@@ -4,7 +4,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import Navigator from './src/Navigator';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Navigator);
